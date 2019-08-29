ec6da7fe6a803bea38c98cbdf3c5c6c1166fc286
Aditya Kumar <k.aditya00@gmail.com>
2019-08-29 18:08:30 +0530

    [Feat] Config file for i3 & i3lock

---
+++ i3lock/final-lock.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+curr_dir=/home/adityaa30/Documents/Projects/Others/my-scripts/i3lock
+temp_background=$curr_dir/temp.png
+lock_image=$curr_dir/lock.png
+
+resolution=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
+
+ffmpeg -f x11grab -video_size $resolution -y -i $DISPLAY -i $lock_image -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $temp_background -loglevel quiet 
+
+# Lock the screen
+i3lock -i $temp_background
+rm $temp_background
