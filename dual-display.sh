dfbab3fd2ebbb875fbf0f8ea02122caa052c7477
Aditya <k.aditya00@gmail.com>
2020-03-19 16:10:10 +0530

    [update] dual-display script

--- dual-display.sh
+++ dual-display.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Adds support for extra monitor places above the laptop's screen in i3wm
+# Check if arguments passed are OK
 
-LAPTOP='eDP-1-1'
-MONITOR='HDMI-0'
+if [ $# != 2 ]
+then
+    echo "Usage: ./dual-display.sh <LAPTOP> <MONITOR>"
+    exit
+fi
+
+# Adds support for extra monitor places above the laptop's screen in i3wm
+LAPTOP=$1
+MONITOR=$2
 
 xrandr --output $MONITOR --mode 1920x1080 --rate 60
 xrandr --output $LAPTOP --mode 1920x1080 --rate 60
